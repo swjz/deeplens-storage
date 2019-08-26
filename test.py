c7583a097eb8c951c80373eda3f84195c119cf8a
sanjay krishnan <sanjaykrishnan@sanjays-MacBook-Air.local>
2019-08-26 14:45:30 -0500

    Updated the api added more details

---
+++ test.py
@@ -0,0 +1,11 @@
+from dlstorage.filesystem.videoio import *
+from dlstorage.constants import *
+from dlstorage.stream import *
+from dlstorage.xform import *
+from dlstorage.header import *
+
+v = VideoStream(0, 300)
+v = v[TestTagger()]
+
+write_video_clips(v, 'bear', MP4V, ObjectHeader(), 50)
+print(read_if('bear', lambda x: True ))
