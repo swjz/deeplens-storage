ff482d8626c5b182b1913127efcb0624fd2d2a24
sanjay krishnan <sanjaykrishnan@sanjays-MacBook-Air.local>
2019-09-05 11:22:24 -0500

    Clean up and committing new code

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ from dlstorage.utils.benchmark import *
 
 f = FileSystemStorageManager(TestTagger(), 'videos')
 p = PerformanceTest(f, '/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4')
-p.getClipSizeTenSec()
-#p.runAll()
+p.runAll()
 
 
