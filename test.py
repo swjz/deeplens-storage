dc8f5e83fec86a4f10e09c8e46de6c9115344d69
primkey7607 <psubramaniam@uchicago.edu>
2019-09-05 14:51:43 -0500

    removed filesys test

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ from dlstorage.filesystem.manager import *
 from dlstorage.constants import *
 from dlstorage.utils.debug import *
 
+"""
 f = FileSystemStorageManager(TestTagger(), 'videos')
 f.put('/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4', 'bunny')
 print(f.get('bunny', TRUE, 60*30))
-
+"""
 vd = VDMSStorageManager(TestTagger())
 vd.put('enter actual directory here', 'desired name')
 print(vd.get('desired name', TRUE, 438))
