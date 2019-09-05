80aa3ea0ad8babf9699c3c3687a6ec6980be0c6d
P.M.Sub <psubramaniam@uchicago.edu>
2019-09-05 10:27:42 -0500

    completed VDMS put() function

--- test.py
+++ test.py
@@ -6,4 +6,7 @@ f = FileSystemStorageManager(TestTagger(), 'videos')
 f.put('/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4', 'bunny')
 print(f.get('bunny', TRUE, 60*30))
 
+vd = VDMSStorageManager(TestTagger())
+vd.put('enter actual directory here', 'desired name')
+print(vd.get('desired name', TRUE, 438))
 
