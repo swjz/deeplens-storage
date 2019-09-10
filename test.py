809009b541c48a7a3f11d190955747a694e33b98
Sanjay Krishnan <sanjaykrishn@gmail.com>
2019-09-10 09:29:05 -0500

    Update test.py
    Fixing a bad merge

--- test.py
+++ test.py
@@ -1,20 +1,6 @@
 from dlstorage.utils.benchmark import *
 
-
-
-"""
-f = FileSystemStorageManager(TestTagger(), 'videos')
-<<<<<<< HEAD
-f.put('/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4', 'bunny')
-print(f.get('bunny', TRUE, 60*30))
-"""
 vd = VDMSStorageManager(TestTagger())
 vd.put('enter actual directory here', 'desired name')
 print(vd.get('desired name', TRUE, 438))
-=======
-p = PerformanceTest(f, '/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4')
-p.getParaTenTenSec()
-#p.runAll()
-
->>>>>>> origin/master
 
