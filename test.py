351d9a723ea7618b7c5e22b88dda305f6fe876fb
primkey7607 <psubramaniam@uchicago.edu>
2019-09-10 14:07:32 -0500

    changed constants, directories, for Ubuntu

--- test.py
+++ test.py
@@ -2,19 +2,21 @@ from dlstorage.utils.benchmark import *
 
 
 
-"""
+
 f = FileSystemStorageManager(TestTagger(), 'videos')
-<<<<<<< HEAD
-f.put('/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4', 'bunny')
-print(f.get('bunny', TRUE, 60*30))
+#f.put('BigBuckBunny.mp4', 'bunny')
+#print(f.get('bunny', TRUE, 60*30))
 """
 vd = VDMSStorageManager(TestTagger())
 vd.put('enter actual directory here', 'desired name')
 print(vd.get('desired name', TRUE, 438))
-=======
-p = PerformanceTest(f, '/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4')
-p.getParaTenTenSec()
+"""
+#These tests are complete, which is why they are commented out
+#p = PerformanceTest(f, 'TrafficTimeLapse.mp4')
+#p.getParaTenTenSec()
 #p.runAll()
 
->>>>>>> origin/master
-
+vd = VDMSStorageManager(TestTagger())
+p2 = PerformanceTest(vd, 'TrafficTimeLapse.mp4')
+#p2.getParaTenTenSec()
+p2.runAll()
