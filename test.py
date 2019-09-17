9baa4ed1388b0e62fae18d45f5a84aa96f757255
primkey7607 <psubramaniam@uchicago.edu>
2019-09-17 12:05:40 -0500

    changed number of cores

--- test.py
+++ test.py
@@ -5,19 +5,10 @@ f = FileSystemStorageManager(TestTagger(), 'videos')
 #<<<<<<< HEAD
 #f.put('BigBuckBunny.mp4', 'bunny')
 #print(f.get('bunny', TRUE, 60*30))
-"""
-vd = VDMSStorageManager(TestTagger())
-vd.put('enter actual directory here', 'desired name')
-print(vd.get('desired name', TRUE, 438))
-"""
-#These tests are complete, which is why they are commented out
-#p = PerformanceTest(f, 'f20sec.mp4')
+#=======
+#p = PerformanceTest(f, 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
 #p.getParaTenTenSec()
 #p.runAll()
-#=======
-p = PerformanceTest(f, 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
-#p.getParaTenTenSec()
-p.runAll()
 #>>>>>>> master
 
 vd = VDMSStorageManager(TestTagger())
