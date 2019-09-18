0b51273b9221f647547c223f50afac8fb7c16620
primkey7607 <psubramaniam@uchicago.edu>
2019-09-18 00:49:02 -0500

    removed unnecessary tests

--- test.py
+++ test.py
@@ -1,18 +1,6 @@
 from dlstorage.utils.benchmark import *
 from dlstorage.utils.vdmsbench import *
 
-f = FileSystemStorageManager(TestTagger(), 'videos')
-#<<<<<<< HEAD
-#f.put('BigBuckBunny.mp4', 'bunny')
-#print(f.get('bunny', TRUE, 60*30))
-#=======
-#p = PerformanceTest(f, 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
-#p.getParaTenTenSec()
-#p.runAll()
-#>>>>>>> master
-
 vd = VDMSStorageManager(TestTagger())
-#p2 = VDMSPerfTest(vd, 'TrafficTimeLapse.mp4')
-p2 = VDMSPerfTest(vd, 'f60sec.mp4')
-#p2.getParaTenTenSec()
+p2 = VDMSPerfTest(vd, 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
 p2.runAll()
