90ef03b1834bf943a6368d4915369471d42ad12b
primkey7607 <psubramaniam@uchicago.edu>
2019-09-19 11:09:57 -0500

    minor bug fixes

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ from dlstorage.utils.benchmark import *
 from dlstorage.utils.vdmsbench import *
 
 vd = VDMSStorageManager(TestTagger())
-p2 = VDMSPerfTest(vd, 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
+#p2 = VDMSPerfTest(vd, 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
+p2 = VDMSPerfTest(vd, 'f65sec.mp4')
 p2.runAll()
